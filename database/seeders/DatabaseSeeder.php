df1742ef1e9fa5e357073f1af498055d6923b659
logan <loganlenevez@gmail.com>
2021-11-24 13:46:53 +0100

    Removing database files

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+use Illuminate\Database\Seeder;
+
+
+
+class DatabaseSeeder extends Seeder
+
+{
+
+    /**
+
+     * Seed the application's database.
+
+     *
+
+     * @return void
+
+     */
+
+    public function run()
+
+    {
+
+        $this->call(UsersTableSeeder::class);
+
+    }
+
+}
