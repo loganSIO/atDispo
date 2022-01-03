a839b1b749d8cad6347fdf7ad9038f974980c080
logan <loganlenevez@gmail.com>
2022-01-03 16:41:23 +0100

    Adding a fr.json to translate auth laravel

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé un e-mail avec un lien de réinitialisation de mot de passe.',
+    'throttled' => 'Attendez un moment avant de réessayer.',
+    'token' => 'Le token de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne trouvons pas d\'utilisateur avec cette adresse e-mail.",
 
 ];
