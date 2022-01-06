718240902ae24bc8f9d98c4c27791ecf7c593f56
logan <loganlenevez@gmail.com>
2022-01-06 23:05:27 +0100

    Update links

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -2,4 +2,4 @@ Bonjour Administrateur,
 <br><br>
 L'utilisateur : {{$email_data['name']}} / {{$email_data['email']}} vient de se créer un compte sur {{ config('app.name', 'Ah ! t\'es dispo ?') }}.
 <br><br>
-Clique <a href="{{ config('app.url') }}/verify?code={{$email_data['verification_code']}}">ici</a> pour valider son compte afin qu'il ait accès à ton calendrier de disponibilités !
+Clique <a href="http://atdispo.herokuapp.com/verify?code={{$email_data['verification_code']}}">ici</a> pour valider son compte afin qu'il ait accès à ton calendrier de disponibilités !
